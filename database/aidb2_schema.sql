1c0859f9664869eb4a4d2572eb7110fe3f6e784a
tassun <32536248+tassun@users.noreply.github.com>
2024-03-18 15:23:27 +0700

    update database table schema

--- database/aidb2_schema.sql
+++ database/aidb2_schema.sql
@@ -6,10 +6,10 @@ CREATE TABLE IF NOT EXISTS `train_course` (
 
 CREATE TABLE IF NOT EXISTS `train_register` (
   `schedule_id` varchar(50) NOT NULL COMMENT 'train_schedule.schedule_id',
-  `trainee_id` varchar(50) NOT NULL COMMENT 'train_trainee.trainee_id',
+  `trainee_id` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'train_trainee.trainee_id',
   `register_date` date NOT NULL,
   `register_time` time NOT NULL,
-  `train_amount` decimal(20,6) NOT NULL DEFAULT (0),
+  `train_amount` decimal(20,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`schedule_id`,`trainee_id`) USING BTREE,
   KEY `FK_train_register_train_trainee` (`trainee_id`),
   CONSTRAINT `FK_train_register_train_schedule` FOREIGN KEY (`schedule_id`) REFERENCES `train_schedule` (`schedule_id`),
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `train_schedule` (
   `end_date` date NOT NULL,
   `end_time` time NOT NULL,
   `train_days` int NOT NULL DEFAULT (0),
-  `train_cost` decimal(20,6) NOT NULL DEFAULT '0.000000',
+  `train_cost` decimal(20,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`schedule_id`),
   KEY `course_id_trainer_id` (`course_id`,`trainer_id`),
   KEY `FK_train_schedule_train_trainer` (`trainer_id`),
