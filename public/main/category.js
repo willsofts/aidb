b05120e85977b1cf36225cdaccd90f0c8ced02b5
tassun <32536248+tassun@users.noreply.github.com>
2024-03-23 16:42:30 +0700

    classify category separately

---
+++ public/main/category.js
@@ -0,0 +1,24 @@
+		const system_categories = {
+			"AIDB1": {
+				title: "Product Selling",
+				examples: [
+					{"quest":"What is the cheapest product name"},
+					{"quest":"What is the most expensive product name"},
+					{"quest":"List product with name and price then order by price descending"},
+					{"quest":"Find out best seller 5 product's name of unit in March,2024"},
+					{"quest":"Find out top 5 customer's name of order amount in March,2024"}
+				],
+				selected: true,
+			},
+			"AIDB2": {
+				title: "Course Training",
+				examples: [
+					{"quest":"What is the cheapest course in training schedule"},
+					{"quest":"What is the most expensive course in training schedule"},
+					{"quest":"List all course name and cost from training schedule"},
+					{"quest":"Find out registered trainee's name in March,2024"},
+					{"quest":"Find out top most training days from training schedule"}
+				],
+				selected: false,
+			},
+		}
