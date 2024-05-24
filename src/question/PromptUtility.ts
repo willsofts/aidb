6088add7072d5d3efbf044e9ccd79944fd8751fa
tassun <32536248+tassun@users.noreply.github.com>
2024-05-24 09:25:01 +0700

    add current date info into prompt

--- src/question/PromptUtility.ts
+++ src/question/PromptUtility.ts
@@ -1,3 +1,5 @@
+import { Utilities } from "@willsofts/will-util";
+
 const ANSWER_PROMPT_INFO = `If result not found then return No result.
         When the question ask about the object, use the object name not object id.`;
 
@@ -10,7 +12,12 @@ export class PromptUtility {
         }
     }
 
+    public getCurrentDate() : string {
+        return Utilities.getLongDate();
+    }
+
     public createChatPrompt(input: string,table_info: string, dialect: string = this.dialect) : string {
+        let current_date = this.getCurrentDate();
         return `Given an input question, first create a syntactically correct ${dialect} query to run return as the answer.
         Use the following format:
         
@@ -22,12 +29,14 @@ export class PromptUtility {
         ${table_info}
                 
         Always using alias name or full table name within columns in query statement and avoid field list is ambiguous.
-        If someone asks for the table foobar, they really mean the product table.
+        If someone asks for the table foobar, they really mean the product table. 
+        For additional information, please refer to the current date is ${current_date}.
                 
         `;
     }
 
     public createQueryPrompt(input: string,table_info: string, dialect: string = this.dialect) : string {
+        let current_date = this.getCurrentDate();
         return `Given an input question, first create a syntactically correct ${dialect} query to run return as the answer.
         Use the following format:
         
@@ -40,6 +49,7 @@ export class PromptUtility {
                 
         Always using alias name or full table name within columns in query statement and avoid field list is ambiguous.
         If someone asks for the table foobar, they really mean the product table.
+        For additional information, please refer to the current date is ${current_date}.
                 
         Question: ${input}`;
     }
@@ -60,12 +70,15 @@ export class PromptUtility {
     }
        
     public createAskPrompt(input: string) : string {
+        let current_date = this.getCurrentDate();
         return `Given an input question then return the answer.
         Use the following format:
         
         Question: "Question here"
         Answer: "An answer in double quotes"
         
+        For additional information, please refer to the current date is ${current_date}.
+
         Question: ${input}`;
     }
 
@@ -86,6 +99,7 @@ export class PromptUtility {
 
     public createDocumentPrompt(input: string, document_info: string, prompt_info: string|null|undefined = "") : string {
         if(!prompt_info || prompt_info==null) prompt_info = "";
+        let current_date = this.getCurrentDate();
         return `Given an input question then return the answer.
         Use the following format:
         
@@ -93,16 +107,19 @@ export class PromptUtility {
         Answer: "An answer in double quotes"
         
         Using only the following information to answer.
-
+        
         ${document_info}
 
         ${prompt_info}
+
+        For additional information, please refer to the current date is ${current_date}.
 
         Question: ${input}`;
     }
 
     public createChatDocumentPrompt(document_info: string, prompt_info: string|null|undefined = "") : string {
         if(!prompt_info || prompt_info==null) prompt_info = "";
+        let current_date = this.getCurrentDate();
         return `Given an input question then return the answer.
         Use the following format:
         
@@ -114,6 +131,8 @@ export class PromptUtility {
         ${document_info}
 
         ${prompt_info}
+
+        For additional information, please refer to the current date is ${current_date}.
         `;
     }
 
