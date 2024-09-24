55cae4ca325ace00dfe550602fd0d044324d7e44
tassun <32536248+tassun@users.noreply.github.com>
2024-09-24 09:54:24 +0700

    update table schema

--- database/aidb_schema.sql
+++ database/aidb_schema.sql
@@ -1,44 +1,135 @@
-CREATE TABLE IF NOT EXISTS `cust_info` (
-  `customer_id` varchar(50) NOT NULL COMMENT 'customer id',
-  `customer_name` varchar(50) NOT NULL COMMENT 'customer name',
-  `customer_surname` varchar(50) NOT NULL COMMENT 'customer surname',
-  PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB COMMENT='table keep customer information';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS `cust_order` (
-  `order_id` varchar(50) NOT NULL COMMENT 'order id',
-  `customer_id` varchar(50) NOT NULL COMMENT 'customer id from table cust_info.customer_id',
-  `order_date` date NOT NULL COMMENT 'order date',
-  `order_time` time NOT NULL COMMENT 'order time',
-  `order_status` varchar(50) DEFAULT NULL,
-  `order_total_unit` bigint NOT NULL DEFAULT (0) COMMENT 'order total unit',
-  `order_total_amount` decimal(20,2) NOT NULL COMMENT 'order total amount',
-  PRIMARY KEY (`order_id`,`customer_id`) USING BTREE
-) ENGINE=InnoDB COMMENT='table keep order master';
+CREATE DATABASE IF NOT EXISTS `aidb` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aidb`;
 
-CREATE TABLE IF NOT EXISTS `cust_order_detail` (
-  `order_id` varchar(50) NOT NULL COMMENT 'order id from table cust_order.order_id',
-  `product_id` varchar(50) NOT NULL COMMENT 'product id from table cust_product.product_id',
-  `order_date` date NOT NULL COMMENT 'order date',
-  `order_time` time NOT NULL COMMENT 'order time',
-  `order_unit` bigint NOT NULL COMMENT 'order unit',
-  `order_price` decimal(20,2) NOT NULL COMMENT 'order price',
-  `order_discount` decimal(20,2) NOT NULL,
-  `order_amount` decimal(20,2) NOT NULL COMMENT 'order amount',
-  PRIMARY KEY (`order_id`,`product_id`)
-) ENGINE=InnoDB COMMENT='table keep product under order from table torder';
+CREATE TABLE IF NOT EXISTS `tagent` (
+  `agentid` varchar(10) NOT NULL,
+  `nameen` varchar(50) NOT NULL,
+  `nameth` varchar(50) NOT NULL,
+  `seqno` int NOT NULL,
+  PRIMARY KEY (`agentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep ai agent';
 
-CREATE TABLE IF NOT EXISTS `cust_product` (
-  `product_id` varchar(50) NOT NULL COMMENT 'product id',
-  `product_name` varchar(50) DEFAULT NULL COMMENT 'product name',
-  `product_price` decimal(16,2) DEFAULT NULL COMMENT 'product price',
-  `product_index` int DEFAULT NULL,
-  PRIMARY KEY (`product_id`) USING BTREE
-) ENGINE=InnoDB COMMENT='table keep product information';
+CREATE TABLE IF NOT EXISTS `tattachfile` (
+  `attachid` varchar(50) NOT NULL,
+  `attachno` varchar(50) NOT NULL,
+  `attachtype` varchar(10) NOT NULL,
+  `attachfile` varchar(150) NOT NULL,
+  `sourcefile` varchar(150) NOT NULL,
+  `attachdate` date NOT NULL,
+  `attachtime` time NOT NULL,
+  `attachmillis` bigint NOT NULL,
+  `attachuser` varchar(50) DEFAULT NULL,
+  `attachremark` varchar(250) DEFAULT NULL,
+  `mimetype` varchar(50) DEFAULT NULL,
+  `attachgroup` varchar(50) DEFAULT NULL,
+  `attachpath` varchar(350) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `attachurl` varchar(250) DEFAULT NULL,
+  `attachsize` bigint DEFAULT NULL,
+  `attachstream` longtext,
+  PRIMARY KEY (`attachid`),
+  KEY `attachno` (`attachno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep attach file';
 
+CREATE TABLE IF NOT EXISTS `tdialect` (
+  `dialectid` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `dialectalias` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `dialecttitle` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `dialectname` varchar(50) NOT NULL,
+  `providedflag` varchar(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0',
+  `urlflag` varchar(1) NOT NULL DEFAULT '1',
+  `seqno` int NOT NULL DEFAULT (0),
+  `dialectoptions` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  PRIMARY KEY (`dialectid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep database dialect info';
 
-Use the following tables relationship:
+CREATE TABLE IF NOT EXISTS `tforum` (
+  `forumid` varchar(50) NOT NULL,
+  `forumcode` varchar(50) NOT NULL,
+  `forumtitle` varchar(50) NOT NULL,
+  `forumgroup` enum('DB','DOC','NOTE') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT 'DB' COMMENT 'DB=Database,DOC=Document',
+  `forumtype` enum('DB','API') NOT NULL DEFAULT 'DB' COMMENT 'DB=Direct Access Database, API=API Service',
+  `forumdialect` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'tdialect.dialectid',
+  `forumapi` varchar(200) DEFAULT NULL,
+  `forumurl` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `forumuser` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `forumpassword` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `forumdatabase` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `forumdbversion` varchar(250) DEFAULT NULL,
+  `forumhost` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `forumport` int DEFAULT '0',
+  `forumselected` varchar(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT '0',
+  `forumsetting` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `forumtable` text,
+  `forumremark` text,
+  `forumprompt` text,
+  `inactive` varchar(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT '0' COMMENT '1=Inactive',
+  `createdate` date DEFAULT NULL,
+  `createtime` time DEFAULT NULL,
+  `createmillis` bigint DEFAULT NULL,
+  `createuser` varchar(50) DEFAULT NULL,
+  `editflag` varchar(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '1' COMMENT '1=Can Edit',
+  `editdate` date DEFAULT NULL,
+  `edittime` time DEFAULT NULL,
+  `editmillis` bigint DEFAULT NULL,
+  `edituser` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`forumid`),
+  KEY `forumcode` (`forumcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep forum information';
 
-cust_order.customer_id = cust_info.customer_id
-cust_order_detail.order_id = cust_order.order_id
-cust_order_detail.product_id = cust_product.product_id
+CREATE TABLE IF NOT EXISTS `tforumagent` (
+  `forumid` varchar(50) NOT NULL,
+  `agentid` varchar(10) NOT NULL,
+  `forumtable` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `forumprompt` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `forumremark` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `createdate` date DEFAULT NULL,
+  `createtime` time DEFAULT NULL,
+  `createmillis` bigint DEFAULT NULL,
+  `createuser` varchar(50) DEFAULT NULL,
+  `editdate` date DEFAULT NULL,
+  `edittime` time DEFAULT NULL,
+  `editmillis` bigint DEFAULT NULL,
+  `edituser` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`forumid`,`agentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep forum setting by agent';
+
+CREATE TABLE IF NOT EXISTS `tforumquest` (
+  `forumid` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'tforum.forumid',
+  `questid` varchar(50) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `seqno` int NOT NULL DEFAULT (0),
+  PRIMARY KEY (`questid`),
+  KEY `forumid` (`forumid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table keep example question';
+
+CREATE TABLE IF NOT EXISTS `ttextconfig` (
+  `docid` varchar(50) NOT NULL,
+  `doctitle` varchar(100) NOT NULL,
+  `docfile` varchar(50) DEFAULT NULL,
+  `inactive` varchar(1) DEFAULT '0' COMMENT '1=Inactive',
+  `captions` text,
+  `createdate` date DEFAULT NULL,
+  `createtime` time DEFAULT NULL,
+  `createmillis` bigint DEFAULT NULL,
+  `createuser` varchar(50) DEFAULT NULL,
+  `editdate` date DEFAULT NULL,
+  `edittime` time DEFAULT NULL,
+  `editmillis` bigint DEFAULT NULL,
+  `edituser` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`docid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='table text document configuration';
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
